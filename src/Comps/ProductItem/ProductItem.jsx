0768a6d436da44aea0a291c0886062fe2e0918b1
santiago <santiagodeandrade4@gmail.com>
2023-01-19 22:07:55 -0400

    Seccion de productos modificada parcialmente

--- src/Comps/ProductItem/ProductItem.jsx
+++ src/Comps/ProductItem/ProductItem.jsx
@@ -8,8 +8,8 @@ export function ProductItem({img, name, descr, backimg}){
                 <img src={img} alt="Tenememos los productos de la mayor calidad de toda venezuela" className="product-item__img"/>
                 <img src={backimg} alt="Tenemos los productos de mayor calidad de toda venezuela" className="product-item__backimg"/>
             </div>
-            <h2 className="product-item__name">{name}</h2>
-            <p className="product-item__paraph">{descr}</p>
+            {/* <h2 className="product-item__name">{name}</h2>
+            <p className="product-item__paraph">{descr}</p> */}
         </div>
     )
 }
--- src/Comps/TopLogo/func/topLogoScrollEfect.js
+++ src/Comps/TopLogo/func/topLogoScrollEfect.js
@@ -4,9 +4,9 @@ export function topLogoScrollEfect(){
     const topLogo = document.getElementsByClassName(topLogoClassName)[0]
     window.addEventListener("scroll", (e)=>{
         if(window.scrollY === 0){
-            topLogo.style.backgroundColor = "inherit"
+            topLogo.style.top = "0vh"
         } else {
-            topLogo.style.backgroundColor = "#555"
+            topLogo.style.top = "-20vh"
         }
     })
 }
