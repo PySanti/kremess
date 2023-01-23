e77fec37b906ba342316578765d42b5a1b025380
santiago <santiagodeandrade4@gmail.com>
2023-01-23 08:40:32 -0400

    Efecto parallax del home aplicado con exito

--- src/Sections/Home/Home.jsx
+++ src/Sections/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./HomeStyles.css"
 export function Home(){
     return (
         <header className="home-container container">
-            <img src="./images/Empanadas/1.jpg" className="home-container__backimg" alt=""/>
+            <div  className="home-container__backimg" alt=""/>
             <p className="home-container__paraph">
                 "Mas de 20 años alimentando los hogares venezolanos"
             </p>
