bec80bc170682c504b512cee4e17db5805d718ff
santiago <santiagodeandrade4@gmail.com>
2023-01-24 22:14:38 -0400

    Mejora de imagenes de productItem

--- src/Comps/ProductItem/ProductItem.jsx
+++ src/Comps/ProductItem/ProductItem.jsx
@@ -5,13 +5,13 @@ import "./ProductItemStyles.css"
 export function ProductItem({img, name, descr, type}){
     const style = {
         backgroundImage : `url(${img})`,
-        backgroundSize : "contain"
+        backgroundSize : "cover"
     }
     return (
         <div className={"product-item t"+type} >
             <div style={style} alt="Tenememos los productos de la mayor calidad de toda venezuela" className="product-item__img parallax"/>
             <div className="product-item__content">
-                <h2 className="product-item__name">{name}</h2>
+                <h2 className="product-item__name title">{name}</h2>
                 <p className="product-item__paraph">{descr}</p>
             </div>
         </div>
