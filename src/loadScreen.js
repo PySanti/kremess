5cb94ea768970fd9614cd94b83336ba214f9b557
santiago <santiagodeandrade4@gmail.com>
2023-01-26 15:50:40 -0400

    Modificacion de loader

--- src/loadScreen.js
+++ src/loadScreen.js
@@ -3,7 +3,7 @@ import { bodyLoadedClassName, loaderClassName, loaderDEACTIVATEDClassName, mainC
 export function loadScreen(){
     const loader = document.getElementsByClassName(loaderClassName)[0]
     const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
-    window.addEventListener("click", ()=>{
+    window.addEventListener("load", ()=>{
         document.getElementsByTagName("body")[0].classList.add(bodyLoadedClassName)
         loader.classList.add(loaderDEACTIVATEDClassName)
         mainContainer.classList.add(mainContainerActivated)
