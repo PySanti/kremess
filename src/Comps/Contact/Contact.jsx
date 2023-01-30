fa823b099ca55bd7e68fd7129628c42821e7adef
santiago <santiagodeandrade4@gmail.com>
2023-01-30 08:28:44 -0400

    Conversion de imagenes

--- src/Comps/Contact/Contact.jsx
+++ src/Comps/Contact/Contact.jsx
@@ -5,17 +5,17 @@ export function Contact(props){
     return (
         <div className="contact">
             <div className="contact-item">
-                <img className="phone-logo icon" src="./icons/phone.png"/>
+                <img className="phone-logo icon" src="./icons/phone.webp"/>
                 <div className="phone-number contact"> 
                     +58 412-7273323 
                 </div>
             </div>
             <div className="contact-item">
-                <img className="email-logo icon" src="./icons/email.png"/>
+                <img className="email-logo icon" src="./icons/email.webp"/>
                 <div className="email-dir contact">mogasi2003@gmail.com</div>
             </div>
             <div className="contact-item">
-                <img className="ig-logo icon" src="./icons/ig.png"/>
+                <img className="ig-logo icon" src="./icons/ig.webp"/>
                 <a className="ig-dir contact"href="https://www.instagram.com/productoskremess">@productoskremess</a>
             </div>
         </div>
