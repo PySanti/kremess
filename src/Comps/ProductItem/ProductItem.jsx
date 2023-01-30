b3c8138f7835165357b948e2760d6484b18ceb3f
santiago <santiagodeandrade4@gmail.com>
2023-01-30 19:17:25 -0400

    Modificaciones del texto de product item

--- src/Comps/ProductItem/ProductItem.jsx
+++ src/Comps/ProductItem/ProductItem.jsx
@@ -5,7 +5,7 @@ import "./ProductItemStyles.css"
 
 export function ProductItem({img, name, descr, type, backimg}){
     return (
-        <div  className={"product-item t"+type} >
+        <div  className={`product-item t${type}`} >
             <img alt="Tenememos los productos de la mayor calidad de toda venezuela" src={backimg} className="product-item__backimg"/>
             <img src={img} alt="Tenememos los productos de la mayor calidad de toda venezuela" className="product-item__img"/>
             <div className="product-item__content">
