acb6adb3c4fb098c238d42d2598e6ba111e5a08d
santiago <santiagodeandrade4@gmail.com>
2023-01-30 09:06:12 -0400

    Li agregados a los clients, loader modificado

--- src/Comps/Client/Client.jsx
+++ src/Comps/Client/Client.jsx
@@ -5,7 +5,7 @@ import "./ClientStyles.css"
 
 export function Client({img, re}){
     return (
-        <a className="client-ref" href={re} >
+        <a className="client-ref" target="__BLANK" href={re} >
             <img className="client__logo" src={img} alt="Estamos ubicados en casi todos los mercados de Caracas."/>
         </a>
     )
--- src/Comps/Contact/Contact.jsx
+++ src/Comps/Contact/Contact.jsx
@@ -5,17 +5,17 @@ export function Contact(props){
     return (
         <div className="contact">
             <div className="contact-item">
-                <img className="phone-logo icon" src="./icons/phone.webp"/>
+                <img className="phone-logo icon" src="./icons/phone.webp" alt="Movil : "/>
                 <div className="phone-number contact"> 
                     +58 412-7273323 
                 </div>
             </div>
             <div className="contact-item">
-                <img className="email-logo icon" src="./icons/email.webp"/>
+                <img className="email-logo icon" src="./icons/email.webp" alt="Email : "/>
                 <div className="email-dir contact">mogasi2003@gmail.com</div>
             </div>
             <div className="contact-item">
-                <img className="ig-logo icon" src="./icons/ig.webp"/>
+                <img className="ig-logo icon" src="./icons/ig.webp" alt="Instagram : "/>
                 <a className="ig-dir contact"href="https://www.instagram.com/productoskremess">@productoskremess</a>
             </div>
         </div>
