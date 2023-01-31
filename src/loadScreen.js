16a5c0cfed6929b0b4a94ef224f4253305ec49f7
santiago <santiagodeandrade4@gmail.com>
2023-01-31 10:53:17 -0400

    actualizacion

--- src/loadScreen.js
+++ src/loadScreen.js
@@ -1,14 +1,15 @@
 import { bodyLoadedClassName, loaderClassName, loaderDEACTIVATEDClassName, mainContainerActivated, mainContainerClassName } from "."
 
 export function loadScreen(){
-    setInterval(()=>{
-        console.log(`ready state ${document.readyState}`)
+    let intervalContainer = setInterval(()=>{
+        if (document.readyState === "complete"){
+                console.log("Hola")
+                const loader = document.getElementsByClassName(loaderClassName)[0]
+                const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
+                document.getElementsByTagName("body")[0].classList.add(bodyLoadedClassName)
+                loader.classList.add(loaderDEACTIVATEDClassName)
+                mainContainer.classList.add(mainContainerActivated)
+                clearInterval(intervalContainer)
+        }
     },100)
-    const loader = document.getElementsByClassName(loaderClassName)[0]
-    const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
-    window.addEventListener("load", ()=>{
-        document.getElementsByTagName("body")[0].classList.add(bodyLoadedClassName)
-        loader.classList.add(loaderDEACTIVATEDClassName)
-        mainContainer.classList.add(mainContainerActivated)
-    })
 }
