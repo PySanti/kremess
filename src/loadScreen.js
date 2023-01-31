a93e678d4df3f91b0350995b195eaa261c63ce88
santiago <santiagodeandrade4@gmail.com>
2023-01-30 20:37:18 -0400

    actualizacion

--- src/loadScreen.js
+++ src/loadScreen.js
@@ -4,7 +4,6 @@ export function loadScreen(){
     const loader = document.getElementsByClassName(loaderClassName)[0]
     const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
     window.addEventListener("load", ()=>{
-        alert("pagina cargada")
         document.getElementsByTagName("body")[0].classList.add(bodyLoadedClassName)
         loader.classList.add(loaderDEACTIVATEDClassName)
         mainContainer.classList.add(mainContainerActivated)
