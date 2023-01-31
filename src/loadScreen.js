84516d12190a2819b5b8956bffd210b40491cfdf
santiago <santiagodeandrade4@gmail.com>
2023-01-30 20:35:01 -0400

    actualizacion

--- src/loadScreen.js
+++ src/loadScreen.js
@@ -1,21 +1,12 @@
 import { bodyLoadedClassName, loaderClassName, loaderDEACTIVATEDClassName, mainContainerActivated, mainContainerClassName } from "."
 
-var loaderInterval = setInterval(loadScreen, 100)
-
-function paginaCargada(){
-    if (document.readyState === "loaded" || document.readyState === "interactive" || document.readyState === "complete") {
-        clearInterval(loaderInterval);
-        return true;
-    }
-}
-
 export function loadScreen(){
-    console.log("Hola")
-    if (paginaCargada){
-        const loader = document.getElementsByClassName(loaderClassName)[0]
-        const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
+    const loader = document.getElementsByClassName(loaderClassName)[0]
+    const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
+    window.addEventListener("load", ()=>{
+        alert("pagina cargada")
         document.getElementsByTagName("body")[0].classList.add(bodyLoadedClassName)
         loader.classList.add(loaderDEACTIVATEDClassName)
         mainContainer.classList.add(mainContainerActivated)
-    }
+    })
 }
