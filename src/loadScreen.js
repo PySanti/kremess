234d964f17342e96f932f0088bfa5c0e92b36205
santiago <santiagodeandrade4@gmail.com>
2023-01-30 20:18:48 -0400

    Actualizacion de loader y sombras de fuentes de pi

--- src/loadScreen.js
+++ src/loadScreen.js
@@ -1,11 +1,20 @@
 import { bodyLoadedClassName, loaderClassName, loaderDEACTIVATEDClassName, mainContainerActivated, mainContainerClassName } from "."
 
+var loaderInterval = setInterval(loadScreen, 100)
+
+function paginaCargada(){
+    if (document.readyState === "loaded" || document.readyState === "interactive" || document.readyState === "complete") {
+        clearInterval(loaderInterval);
+        return true;
+    }
+}
+
 export function loadScreen(){
-    const loader = document.getElementsByClassName(loaderClassName)[0]
-    const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
-    window.addEventListener("click", ()=>{
+    if (paginaCargada){
+        const loader = document.getElementsByClassName(loaderClassName)[0]
+        const mainContainer = document.getElementsByClassName(mainContainerClassName)[0]
         document.getElementsByTagName("body")[0].classList.add(bodyLoadedClassName)
         loader.classList.add(loaderDEACTIVATEDClassName)
         mainContainer.classList.add(mainContainerActivated)
-    })
+    }
 }
